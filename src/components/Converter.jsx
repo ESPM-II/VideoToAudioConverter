8dc93fd9a9136c688b2b4e6127d31236d0d2f097
ESPM-II <jjaolivos.02@gmail.com>
2024-05-01 20:43:14 -0400

    feat loader

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 import { fetch } from "../services/Api";
+import { Loader } from "./Loader";
 
 export const Converter = () => {
     const [link, setLink] = useState('');
     const [id, setId] = useState(null);
     const [response, setResponse] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         if(id) {
-        const fetchData =  () => {
-            let interval = setInterval(async function () {
-                const res = await fetch(id);
+            const fetchData = async () => {
+                setIsLoading(true);
+                let interval = setInterval(async function () {
+                    const res = await fetch(id);
 
-                if(res.status === 200 && res.data.status === "ok"){
-                    setResponse(res.data);
-                    clearInterval(interval);
-                } else if (res.status === 200 & res.data.status === "fail"){
-                    alert("Invalid video ID");
-                }
-            }, 1000);
+                    if(res.status === 200 && res.data.status === "ok"){
+                        setResponse(res.data);
+                        clearInterval(interval);
+                        setIsLoading(false);
+                        setLink(''); // Restablecer el estado del enlace
+                        alert("Se descargó el archivo correctamente");
+                        window.location.reload(); // Recargar la página después de la descarga
+                    } else if (res.status === 200 && res.data.status === "fail"){
+                        alert("Invalid video ID");
+                        clearInterval(interval);
+                        setIsLoading(false);
+                    }
+                    
+                }, 1000);
+            }
+
+            fetchData();
         }
-
-        fetchData();
-    }
     }, [id]);
 
     useEffect(() => {
@@ -34,30 +44,32 @@ export const Converter = () => {
     
     return(
         <div className="app">
-        <div id="logo">
-            <h2>YT2MP3</h2> 
-        </div>
+            <div id="logo">
+                <h2>YT2MP3</h2> 
+            </div>
 
-        <div id="body">
-            <input 
-                type="text"
-                placeholder="Pega aquí el link de YouTube"
-                value={link}
-                onChange={(e) => {
-                    setLink(e.target.value);
-                }} 
+            <div id="body">
+                <input 
+                    type="text"
+                    placeholder="Pega aquí el link de YouTube"
+                    value={link}
+                    onChange={(e) => {
+                        setLink(e.target.value);
+                    }} 
                 />
-            <span>Podríamos tardar un momento en convertir tu video a mp3</span>
-        </div>
-        <button
-            onClick={() => {
-                const text = link.split("=")[1];
-                if (text) {
-                    setId(text);
-                }
-            }}
-        >Descargar</button>
+                <span>Podríamos tardar un momento en convertir tu video a mp3</span>
+            </div>
+            <button
+                onClick={() => {
+                    const text = link.split("=")[1];
+                    if (text) {
+                        setId(text);
+                    }
+                }}
+            >
+                Descargar
+            </button>
+            {isLoading && <Loader />}
         </div>
     )
 }
-
